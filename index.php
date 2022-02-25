acfcfc14dfc6171d0fb6ee68be1325f607969a23
Noah2211 <noah.decroix3@gmail.com>
2022-02-25 14:36:32 +0100

    done

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__ . '/Config.php';
+require  __DIR__ . '/DB_Connect.php';
 /*
  * 1 - Uniquement pour la pratique, reproduisez via mysql workbench le schémas proposé.
  * 2 - Exportez le résultat de manière à créer les tables en base de données.
@@ -7,3 +9,17 @@
  * 4 - A l'aide d'un simple print_r, afficher les rôles de chaque utilisateur.
  * 5 - FIN !
  */
+
+$stmt = DB_Connect::dbConnect()->prepare("
+    SELECT user.username, role.role
+    FROM user_role
+        INNER JOIN role ON user_role.role_id = role.id
+        INNER JOIN user ON user_role.user_id = user.id
+");
+
+$stmt->execute();
+
+echo "<pre>";
+print_r($stmt->fetchAll());
+echo "</pre>";
+
